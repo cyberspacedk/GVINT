07b06a7c7c78649119f658d4c3eb2b6f46620300
cyberspacedk <frontend.dk@gmail.com>
2019-02-18 17:44:15 +0200

    add sound files

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
 import "../sass/styles.scss";
-
+import sound from '../audio/Main_Theme/MainTheme.mp3';
 import monsters from './monster-deck';
 import northern from './northern-realms-deck';
+ 
+window.addEventListener('DOMContentLoaded',()=>{ 
+    const mainTheme = new Audio();
+    mainTheme.src = '../audio/Main_Theme/MainTheme.mp3';
+    mainTheme.play(); 
+})

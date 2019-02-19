69964799a35c2fa9111f5d48b3834885ce22928a
Pecherskiy88 <pecherskiy88@gmail.com>
2019-02-19 11:55:13 +0200

    merge, first try

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,6 @@ import "../sass/styles.scss";
 import sound from '../audio/Main_Theme/MainTheme.mp3';
 import monsters from './monster-deck';
 import northern from './northern-realms-deck';
+import {userEnter} from './server';
  
-window.addEventListener('DOMContentLoaded',()=>{ 
-    const mainTheme = new Audio();
-    mainTheme.src = '../audio/Main_Theme/MainTheme.mp3';
-    mainTheme.play(); 
-})
+window.addEventListener('DOMContentLoaded', () => {userEnter()})

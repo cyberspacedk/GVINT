dd650295583104e3c6eae2c2e039080962d3adb9
Alexey <alexey0725@gmail.com>
2019-02-19 12:25:07 +0200

    storage

--- src/js/deck_selection.js
+++ src/js/deck_selection.js
@@ -1,19 +1,15 @@
-import "../sass/st.scss";
 
 
 const cardBox = document.querySelector (".cardBox")
 
 function deckSelections (e) {
     e.preventDefault();
-    let result = e.target.dataset.name;
-    
-    let obj = localStorage.setItem( `result`, JSON.stringify(result));
-   console.log(obj);
-   return;
-   
+    let result = e.target.dataset.name; 
+
+   localStorage.setItem(`name`, JSON.stringify(result));
+
 }
 
 cardBox.addEventListener ("click", deckSelections);
 
 export {deckSelections};
-export default cardBox;
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,3 @@
-import {deckSelections} from './deck_selection'
-import cardBox from './st.scss'
-
-// let body = document.querySelector ('body');
-// body.append(cardBox); 
+import {deckSelections} from './deck_selection';
 
 console.log(deckSelections);

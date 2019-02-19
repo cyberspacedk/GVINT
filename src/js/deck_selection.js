5be4dc146cf8c91730027869c984a08acf3d6aef
Alexey <alexey0725@gmail.com>
2019-02-19 07:57:47 +0200

    Test1

---
+++ src/js/deck_selection.js
@@ -0,0 +1,18 @@
+import "../sass/st.scss";
+
+
+const cardBox = document.querySelector (".cardBox")
+
+function deckSelections (e) {
+    e.preventDefault();
+    let result = e.target.dataset.name;
+    
+    let obj = localStorage.setItem( `result`, JSON.stringify(result));
+   console.log(obj);
+   return;
+   
+}
+
+cardBox.addEventListener ("click", deckSelections);
+
+export {deckSelections};
--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,2 @@
-// При ховері на колоду вона збільшується і виділяється
-// При кліку на колоду вона записується в сторедж
-
- let card = {
-    ability: 'string',
-    name: 'nameCard',
-    positions: [
-        "Melee",
-        "Ranged",
-        "Siege"
-        ],
-    strength: 4,
-    type: "Gold",
-    img: URL,
-    audio: url,
-}
-const cardBox = document.querySelector (".cardBox")
-
-cardBox.addEventListener("mouseover",newCard )
-
-
-function newCard (){
-
-    
-}
+import {deckSelections} from './deck_selection'
 

dbd0bfc8ed4870d14fd594c345a0cb7ba16b9f33
RolandMarkarov <markarovr@gmail.com>
2019-02-20 14:49:57 +0200

    dealing opponents cards

--- src/js/dealingCards.js
+++ src/js/dealingCards.js
@@ -1,7 +1,10 @@
 'use strict';
 
 import '../sass/dealingcards.scss';
-import {cards} from './cards';
+import { cards } from './cards';
+import monsterReverse from '../img/Factions/faction-monsters.png';
+import northernReverse from '../img/Factions/faction-northern-realms.png';
+
 // import DETHMOLD from '../img/NorthernRealms/DETHMOLD.png';
 // import GERALT_IGNI from '../img/NorthernRealms/GERALT_IGNI.png';
 // import KAEDWENI_CAVALRY from '../img/NorthernRealms/KAEDWENI CAVALRY.png';
@@ -9,31 +12,33 @@ import {cards} from './cards';
 // import King_Foltest from '../img/NorthernRealms/King Foltest.png';
 // import PRINCE_STENNIS from '../img/NorthernRealms/PRINCE STENNIS.png';
 
-
-
-
-
-export function dealingCards(obj) {
-	let movedCard = null;
-	let idx = null;
-	let i =0;
-	obj.cardHand = [];
-	while (i < 10) {
-		idx = Math.round(Math.random() * obj.deck.length);
-		movedCard = obj.deck.splice(idx, 1);
-		obj.cardHand.push(movedCard[0]);
-		i ++
-	}
-	putOnBoard(obj.cardHand, '#player-hand')
-	console.log("HAND" ,obj.cardHand);
+export function dealingCards(player, opponent) {
+	let playerHand = fillCardHand(player);
+	let opponentHand = fillCardHand(opponent);
+	putOnBoard(playerHand, '#player-hand');
+	putOnBoard(opponentHand, '#opponent-hand');
 }
-function putOnBoard(arr, container){
+
+function putOnBoard(arr, container) {
 	let reg = /[ \w-]+?(?=\.)/gi;
 	let div = document.querySelector(container);
-	let str = arr.reduce((acc, el)=> 
-		acc + `<div class="card_in_hands">
-		<img src=${cards[el.img.match(reg)[0]]} class="img_size" data-name=${el.name}></div>`, ''
-	)
+	let str = arr.reduce(
+		(acc, el) =>
+			acc +`<div class="card_in_hands"><img src=${"#player-hand" === container? cards[el.img.match(reg)[0]] : monsterReverse} class="img_size" data-name=${el.name}></div>`,'');
 	console.log(str);
 	div.innerHTML = str;
-};
+}
+
+function fillCardHand(userObj) {
+	let movedCard = null;
+	let idx = null;
+	let i = 0;
+	userObj.cardHand = [];
+	while (i < 10) {
+		idx = Math.round(Math.random() * userObj.deck.length);
+		movedCard = userObj.deck.splice(idx, 1);
+		userObj.cardHand.push(movedCard[0]);
+		i++;
+	}
+	return userObj.cardHand;
+}
--- src/js/server.js
+++ src/js/server.js
@@ -1,147 +1,165 @@
 import firebase from './firebase';
-import { renderBattlefield, body } from "./battlefield";
+import { renderBattlefield, body } from './battlefield';
 import { dealingCards } from './dealingCards';
 
-
 export function userEnter() {
-    firebase.auth().signInAnonymously()
-        .then(function (user) {
-            console.log('Logged in as Anonymous!')
-            localStorage.setItem('userID', JSON.stringify(user.user.uid));
-            firebase.database().ref(`user/${JSON.parse(localStorage.getItem('userID'))}`).set({
-                id: JSON.parse(localStorage.getItem('userID'))
-            })
-        }).catch(function (error) {
-            let errorCode = error.code;
-            let errorMessage = error.message;
-            console.log(errorCode);
-            console.log(errorMessage);
-        });
+	firebase
+		.auth()
+		.signInAnonymously()
+		.then(function(user) {
+			console.log('Logged in as Anonymous!');
+			localStorage.setItem('userID', JSON.stringify(user.user.uid));
+			firebase.database().ref(`user/${JSON.parse(localStorage.getItem('userID'))}`).set({
+				id: JSON.parse(localStorage.getItem('userID'))
+			});
+		})
+		.catch(function(error) {
+			let errorCode = error.code;
+			let errorMessage = error.message;
+			console.log(errorCode);
+			console.log(errorMessage);
+		});
 }
 
 export function play() {
-    firebase.database().ref('rooms').once('value')
-        .then(snap => snap.val())
-        .then(rooms => {
-            if (!rooms) {
-                
-                console.log('create first room');
-                readDeck().then(data => createRoom(JSON.parse(localStorage.getItem('userID')),data));
-                return;
-            }
-            let keys = Object.keys(rooms);
-            for (let room of keys) {
-                if (rooms[room].length < 2) {
-                readDeck().then(data => joinToRoom(room,data));
-                    console.log('join');
-                    break;
-                } else {
-                    readDeck().then(data => createRoom(JSON.parse(localStorage.getItem('userID')),data));
-                    console.log('create');
-                }
-            }
-        })
-    firebase.database().ref('rooms').on('child_removed', function (data) {
-        // console.log(localStorage.getItem('roomID'));
-        if (data.key === localStorage.getItem('roomID')) {
-            console.log('Disconected');
-        }
-
-    })
+	firebase.database().ref('rooms').once('value').then((snap) => snap.val()).then((rooms) => {
+		if (!rooms) {
+			console.log('create first room');
+			readDeck().then((data) => createRoom(JSON.parse(localStorage.getItem('userID')), data));
+			return;
+		}
+		let keys = Object.keys(rooms);
+		for (let room of keys) {
+			if (rooms[room].length < 2) {
+				readDeck().then((data) => joinToRoom(room, data));
+				console.log('join');
+				break;
+			} else {
+				readDeck().then((data) => createRoom(JSON.parse(localStorage.getItem('userID')), data));
+				console.log('create');
+			}
+		}
+	});
+	firebase.database().ref('rooms').on('child_removed', function(data) {
+		// console.log(localStorage.getItem('roomID'));
+		if (data.key === localStorage.getItem('roomID')) {
+			console.log('Disconected');
+		}
+	});
 }
 
 export function createRoom(id, deck) {
-    firebase.database().ref(`rooms/${id}`).set([{
-        id: JSON.parse(localStorage.getItem('userID')),
-        name: 'Player 1',
-        deck: deck,
-        cardHand:[],
-        graveyard: [],
-        topRow: [],
-        middleRow: [],
-        bottomRow: [],
-        endRound: false,
-        myTurn: false,
-        victoryCount: 0,
-        topRowSum: 0,
-        middleRowSum: 0,
-        bottomRowSum: 0,
-        total: 0,
-    }])
-    .then(() => listenRoomAdd());
-    localStorage.setItem('roomID', id);
+	firebase
+		.database()
+		.ref(`rooms/${id}`)
+		.set([
+			{
+				id: JSON.parse(localStorage.getItem('userID')),
+				name: 'Player 1',
+				deck: deck,
+				cardHand: [],
+				graveyard: [],
+				topRow: [],
+				middleRow: [],
+				bottomRow: [],
+				endRound: false,
+				myTurn: false,
+				victoryCount: 0,
+				topRowSum: 0,
+				middleRowSum: 0,
+				bottomRowSum: 0,
+				total: 0
+			}
+		])
+		.then(() => listenRoomAdd());
+	localStorage.setItem('roomID', id);
 }
 
 export function joinToRoom(id, deck) {
-    firebase.database().ref(`rooms/${id}`).once('value')
-        .then(snap => snap.val())
-        .then(data => {
-            data.push({
-                id: JSON.parse(localStorage.getItem('userID')),
-                name: 'Player 2',
-                deck: deck,
-                cardHand:[],
-                graveyard: [],
-                topRow: [],
-                middleRow: [],
-                bottomRow: [],
-                endRound: false,
-                myTurn: false,
-                victoryCount: 0,
-                topRowSum: 0,
-                middleRowSum: 0,
-                bottomRowSum: 0,
-                total: 0,
-                
-            })
-            return data
-        })
-        .then(data => firebase.database().ref(`rooms/${id}`).set(data))
-        .then(() => listenRoomAdd())
-    localStorage.setItem('roomID', id);
-
+	firebase
+		.database()
+		.ref(`rooms/${id}`)
+		.once('value')
+		.then((snap) => snap.val())
+		.then((data) => {
+			data.push({
+				id: JSON.parse(localStorage.getItem('userID')),
+				name: 'Player 2',
+				deck: deck,
+				cardHand: [],
+				graveyard: [],
+				topRow: [],
+				middleRow: [],
+				bottomRow: [],
+				endRound: false,
+				myTurn: false,
+				victoryCount: 0,
+				topRowSum: 0,
+				middleRowSum: 0,
+				bottomRowSum: 0,
+				total: 0
+			});
+			return data;
+		})
+		.then((data) => firebase.database().ref(`rooms/${id}`).set(data))
+		.then(() => listenRoomAdd());
+	localStorage.setItem('roomID', id);
 }
 
 export function removeRoom() {
-    firebase.database().ref(`rooms/${localStorage.getItem('roomID')}`).remove();
-    console.log('remove');
+	firebase.database().ref(`rooms/${localStorage.getItem('roomID')}`).remove();
+	console.log('remove');
 }
 
-export function userExit () {
-    removeRoom();
-    alert('Ваш оппонент покинул игру');
+export function userExit() {
+	removeRoom();
+	alert('Ваш оппонент покинул игру');
 }
 
-export function redirectUser () {
-    console.log('asdfdfsdfsdfsdf');
+export function redirectUser() {
+	console.log('asdfdfsdfsdfsdf');
 }
 
-export function listenRoomAdd () {
-    firebase.database().ref(`rooms/${localStorage.getItem('roomID')}`).on('child_added', function(data) {
-        firebase.database().ref(`rooms/${localStorage.getItem('roomID')}`).once('value')
-        .then(snap => snap.val())
-        .then(data=> {
-            if(data.length === 2) {
-                renderBattlefield(body);
-                findUser();
-                    console.log('start');
-                
-            }
-        })
-    })
+export function listenRoomAdd() {
+	firebase.database().ref(`rooms/${localStorage.getItem('roomID')}`).on('child_added', function(data) {
+		firebase
+			.database()
+			.ref(`rooms/${localStorage.getItem('roomID')}`)
+			.once('value')
+			.then((snap) => snap.val())
+			.then((data) => {
+				if (data.length === 2) {
+					renderBattlefield(body);
+					findUser();
+					console.log('start');
+				}
+			});
+	});
 }
 
 // firebase.database().ref('decks').once('value')
 //         .then(snap => console.log(snap.val()))
 
 function readDeck() {
-    return firebase.database().ref(`decks/${JSON.parse(localStorage.getItem('faction'))}`).once('value')
-    .then(snap => snap.val())
+	return firebase
+		.database()
+		.ref(`decks/${JSON.parse(localStorage.getItem('faction'))}`)
+		.once('value')
+		.then((snap) => snap.val());
 }
-function findUser(){
-return firebase.database().ref(`rooms/${(localStorage.getItem('roomID'))}`).once('value')
-.then(snap => snap.val()).then(arr => {
-    let x = arr.find(el => el.id===JSON.parse(localStorage.getItem('userID')))
-return x})
-.then(result => dealingCards(result))
+function findUser() {
+	return firebase
+		.database()
+		.ref(`rooms/${localStorage.getItem('roomID')}`)
+		.once('value')
+		.then((snap) => snap.val())
+		.then((arr) => {
+			let x = arr.find((el) => el.id === JSON.parse(localStorage.getItem('userID')));
+			let y = arr.find((el) => el.id !== JSON.parse(localStorage.getItem('userID')));
+			return { user: x, opponent: y };
+		})
+		.then((result) => {
+            // console.log(result);
+            dealingCards(result.user, result.opponent)
+        })
 }

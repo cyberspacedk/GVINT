fb58d170e290e22c24865b6ddc1ae1021fd12aa3
iRoy <44869601+monolithphoenix@users.noreply.github.com>
2019-02-21 14:02:25 +0200

    add little changes

--- src/js/cardHoverDiscription.js
+++ src/js/cardHoverDiscription.js
@@ -1,17 +1,17 @@
 export
-const hand = document.querySelector('.hand');
-const rightBar = document.querySelector('.rightBar')
+const hand = document.querySelector('#player-hand');
+const rightCard = document.querySelector('.right__card')
 
 hand.addEventListener('mouseenter', showCardDiscription);
 
 function showCardDiscription(ev) {
     if (ev.target.nodeName === 'IMG') {
-        rightBar.innerHTML = `
-        <img src="" alt="">
+        rightCard.innerHTML = `
+        <img src=${ev.target.src} alt="">
         <h2>${characterName}</h2>
         <h3>${characterClass}</h3>
         <p>${discription}</p>`
     }
 }
 
-localStorage.getItem('')
+// userObj.cardHand

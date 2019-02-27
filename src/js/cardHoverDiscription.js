fde3b6170c1b35ca2830ca38f19bfe0681649dbb
iRoy <44869601+monolithphoenix@users.noreply.github.com>
2019-02-27 05:56:36 +0200

    add audio, delete one console.log

--- src/js/cardHoverDiscription.js
+++ src/js/cardHoverDiscription.js
@@ -11,13 +11,12 @@ export class showMoreInfo{
     }
 
     start(users) {
-        console.log(users);
         this.hand.addEventListener("mouseover", (ev) => {
             this.rightCard.classList.add('showOut');
             this.rightCard.classList.remove('showIn');
             this.rightCard.innerHTML = '';
             if (ev.target.nodeName === 'IMG') {
-                console.log(users.user.cardHand.find(el => el.name === ev.target.dataset.name));
+                console.log('mouse on card!');
                 this.rightCard.classList.add('showIn');
                 this.rightCard.classList.remove('showOut');
                 this.rightCard.innerHTML = `
@@ -31,6 +30,7 @@ export class showMoreInfo{
                         <p>${users.user.cardHand.find(el => el.name === ev.target.dataset.name).ability}</p>
                     </div>
                 </div>`
+                new Audio(users.user.cardHand.find(el => el.name === ev.target.dataset.name).audio).play();
             };
         })
     };

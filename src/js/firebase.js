7d5111819ba652e096389266e4d5b7ac31cd0d4b
jackil182 <jackil182@gmail.com>
2019-03-29 21:00:23 +0200

    changed firebase settings, to a new db, deployed to firebase server

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage'
 // Initialize Firebase
-let config = {
-    apiKey: "AIzaSyBClkV0ZBnAvDuoGtwafCHVlyGJnMWKnIo",
-    authDomain: "servertest-e045e.firebaseapp.com",
-    databaseURL: "https://servertest-e045e.firebaseio.com",
-    projectId: "servertest-e045e",
-    storageBucket: "servertest-e045e.appspot.com",
-    messagingSenderId: "212365800760"
- };
- firebase.initializeApp(config);
+const config = {
+  apiKey: "AIzaSyC4rPHEwcuFq5URs-jKkxGdZpKJULIUZOc",
+  authDomain: "gwent-js.firebaseapp.com",
+  databaseURL: "https://gwent-js.firebaseio.com",
+  projectId: "gwent-js",
+  storageBucket: "gwent-js.appspot.com",
+  messagingSenderId: "599400950827"
+};
+firebase.initializeApp(config);
 
  export default firebase;
